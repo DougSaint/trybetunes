ac272005f5fd2a76372a7e21c025d843feb55574
Douglas Santos <ds4208151@gmail.com>
2022-12-13 16:29:55 -0300

    ex 01

---
+++ src/pages/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+  render() {
+    return (<div data-testid="page-not-found"> Notfound </div>);
+  }
+}
+
+export default NotFound;
---
+++ src/pages/ProfileEdit.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class ProfileEdit extends React.Component {
+  render() {
+    return (<div data-testid="page-profile-edit">PRofile EDit </div>);
+  }
+}
+
+export default ProfileEdit;

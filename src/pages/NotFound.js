5051aacb693cc3f5db63699e733df749d2f3b7b3
Douglas Santos <ds4208151@gmail.com>
2023-02-11 03:52:42 -0300

    organizando a notfound

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 class NotFound extends React.Component {
   render() {
-    return (<div data-testid="page-not-found" className="notfound"> Notfound </div>);
+    return (
+      <div data-testid="page-not-found" className="notfound">
+        <h1>404 Error: Page Not Found</h1>
+        <p>Sorry, the page you are looking for does not exist.</p>
+        <iframe
+          src="https://giphy.com/embed/9J7tdYltWyXIY"
+          width="480"
+          height="404"
+          title="notfound gif"
+        />
+      </div>
+    );
   }
 }
 
